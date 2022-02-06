a89a31ffe75f17464e824b5ab711fc81c7a83288
nduginets <nduginets@devexperts.com>
2022-02-06 14:12:54 +0300

    nft mint for tokens

--- deploy/deploy.js
+++ deploy/deploy.js
@@ -1,7 +1,7 @@
 const hre = require("hardhat");
 const {waffle} = require("hardhat");
 
-async function deployNft() {
+async function initialize() {
     let accounts = await hre.ethers.getSigners();
     let provider = waffle.provider;
     console.log("addresses & balances");
@@ -10,15 +10,33 @@ async function deployNft() {
     }
     const owner = accounts[0];
     console.log("=================================");
-    const nftContract = (await hre.ethers.getContractFactory("RabbitsCollection")).connect(owner);
-    const nftDeployed = await nftContract.deploy(this.config.nftDirPath);
-    console.log("nft contract address: " + nftDeployed.address);
-    console.log("owner balance: " + await provider.getBalance(await owner.getAddress()));
-    console.log("Contract name: " + await  nftDeployed.name());
+    return {"owner": owner, "provider": provider};
 }
 
-deployNft()
-    .then(() => process.exit(0))
+async function deployToken(owner, provider) {
+    const tokenContract = (await hre.ethers.getContractFactory("Token")).connect(owner);
+    const tokenDeployed = await tokenContract.deploy();
+    console.log("Token contract address: " + tokenDeployed.address);
+    console.log("Balance after operation: " + await provider.getBalance(await owner.getAddress()));
+    return tokenDeployed.address;
+}
+
+async function deployNft(owner, provider, tokenAddress) {
+    const nftContract = (await hre.ethers.getContractFactory("RabbitsCollection")).connect(owner);
+    const nftDeployed = await nftContract.deploy(tokenAddress, this.config.nftDirPath);
+    console.log("NFT contract address: " + nftDeployed.address);
+    console.log("Balance after operation: " + await provider.getBalance(await owner.getAddress()));
+}
+
+async function deployAll() {
+    let res = await initialize();
+    let owner = res["owner"];
+    let provider = res["provider"];
+    let tokenAddress = await deployToken(owner, provider);
+    await deployNft(owner, provider, tokenAddress);
+}
+
+deployAll().then(() => process.exit(0))
     .catch((error) => {
         console.error(error);
         process.exit(1);
